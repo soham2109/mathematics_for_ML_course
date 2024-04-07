10a0281addfc698c38a16bee6ef0f3026c0530e2
Mohit Agarwala <mohit.agarwala@olaelectric.com>
2024-04-07 15:49:41 +0530

    Adding the header thedeeplearners.com

--- the_deep_learner.py
+++ the_deep_learner.py
@@ -4,7 +4,15 @@ import streamlit as st
 
 class MainApp:
     def __init__(self):
-        st.markdown("Welcome to thedeeplearners.com")
+        self.app()
+    
+    def app(self):
+        st.markdown("<h1 style='text-align: center; color: red;'>thedeeplearners.com</h1>", unsafe_allow_html=True)
+        st.markdown("---")
+        st.markdown("""
+Guidebook  
+----  
+""")
 
 if __name__=="__main__":
     app = MainApp()       
